ad02767dbc734377dcffa9b2b70eaaf58791c94e
Andres29091 <andres29091@gmail.com>
2022-10-05 14:39:13 -0500

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InCar.Data
+{
+  public class ApplicationDbContext: DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options): base (options)
+        {
+
+        }
+
+        //protected override void  OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+        //    modelBuilder.Entity<PeliculasGeneros>()
+        //        .HasKey(x => new { x.GeneroId, x.PeliculaId });
+
+        //    base.OnModelCreating(modelBuilder);
+
+        //}
+
+
+
+        //public DbSet<Genero> Genero { get; set; }
+
+        //public DbSet<Actor> Actores{ get; set; }
+
+        //public DbSet<Pelicula> Pelicula { get; set; }
+
+        //public DbSet<PeliculasGeneros> PeliculasGeneros { get; set; }
+        //public DbSet<IdentityModels> Users { get; set; }
+
+    }
+}
---
+++ Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+
+namespace MedFlix.Helpers
+{
+    public class AutoMapperProfiles: Profile
+    {
+
+        public AutoMapperProfiles()
+        {
+
+
+          
+            //CreateMap<Genero,GeneroDTO >().ReverseMap();
+
+
+            //CreateMap<GeneroCreacionDTO, Genero>();
+
+
+
+            //CreateMap<Actor, ActorDTO>().ReverseMap();
+
+
+            //CreateMap<ActorCreacionDTO, Actor>();
+
+        }
+
+
+
+
+
+
+
+    }
+}
