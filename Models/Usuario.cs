9253fd60de93f93e308ed98a6cdd4e3b3e4b436b
Andres29091 <Andres29091@gmail.com>
2022-10-12 21:46:51 -0500

    - Se crea controller de manejo de usuarios.

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -7,7 +7,6 @@ namespace InCar.Models
 {
   public class Usuario: IdentityUser
   {
-    [Required(ErrorMessage = "El campo {0} es requerido")]
     public int CodigoTipoDocumento { get; set; }
     public string Direccion { get; set; }
     public string Movil { get; set; }
